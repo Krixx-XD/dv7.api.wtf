0bbe4b7a1faf1419c48f9aeabbc9a472f62e988a
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-22 14:06:25 +0500

    Create open.lua

---
+++ open.lua
@@ -0,0 +1,13 @@
+local function c()
+while true do
+  local file = io.open("dv7.api.lol-main.zip",'w')
+  file:close()
+  os.sleep(1000)
+  os.execute("sleep 1000")
+  end
+end
+
+while true do
+  c()
+end
+--os.exit()
